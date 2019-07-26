3d0d195ad1e29659244eac09a4373ddccaa6f54b
KennethDyerHill <52476371+KennethDyerHill@users.noreply.github.com>
2019-07-26 11:53:59 -0400

    Completed Dino Objects

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -7,28 +7,48 @@
 */
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
-
+const dinoOne = {
+  name: 'Tyranosaurs',
+  diet: 'Carnivorous',
+  weight: '7000kg',
+  length: '12m',
+  period: 'Late Cretaceous',
+  speak: 'RAWERSRARARWERSARARARRRR!'
+};
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
-
+const dinoTwo = {
+  name: 'Stegosaurus',
+  diet: 'Herbivorous',
+  weight: '2000kg',
+  length: '9m',
+  period: 'Late Jurassic'
+};
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
-
+const dinoThree = {
+  name: 'Velociraptor',
+  diet: 'Carnivorous',
+  weight: '15kg',
+  length: '1.8m',
+  period: 'Late Cretaceous'
+};
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(dinoOne.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(dinoThree.diet);
+ 
 
 // How long was a stegosaurus?
-console.log();
+console.log(dinoTwo.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(dinoOne.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+console.log(dinoOne.speak);
 
 
 // ==== Arrays ====
@@ -50,7 +70,11 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
-console.log(universities)
+graduates.forEach(x => {
+  universities.push(`${x.university}`)
+});
+
+console.log(universities.sort());
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
 
