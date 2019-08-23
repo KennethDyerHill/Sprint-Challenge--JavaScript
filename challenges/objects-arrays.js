56d4bbeb5f870be3206071cbc8a049d717c6674e
KennethDyerHill <52476371+KennethDyerHill@users.noreply.github.com>
2019-08-23 12:23:26 -0400

    Update

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -90,8 +90,9 @@ console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
-const uni = graduates.filter(x => {
-  return (x.university == 'uni');
+const uni = [];
+  graduates.filter(x => {
+    return (x.university === 'Uni');
 });
 console.log(uni);
 
